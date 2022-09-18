306bacb5e3b11a8b728bd5b8aa96b49a4f81ae70
Mark Dean <mpmdean@gmail.com>
2022-09-18 18:20:27 +0200

    init

---
+++ trigonometry.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+def sin(angle):
+    return np.sin(np.deg2rad(angle))
+
+
+def arccos(value):
+    return np.rad2deg(np.arccos(value))
+
+
+def cos(angle):
+    return np.cos(np.deg2rad(angle))
